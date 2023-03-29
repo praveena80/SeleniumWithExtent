b1c095f356bc63b746899d0fb414492f0dda7657
praveena80 <57277741+praveena80@users.noreply.github.com>
2023-03-29 15:18:23 -0700

    Added more methods

--- src/main/java/util/CommonMethods.java
+++ src/main/java/util/CommonMethods.java
@@ -1,24 +1,31 @@
 package util;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class CommonMethods {
     public WebDriver driver;
     public WebDriverWait wait ;
+    JavascriptExecutor js;
 
     public CommonMethods(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        js = (JavascriptExecutor) driver;
     }
+
+    Actions actions =  new Actions(driver);
 
     public void waitForElement( WebElement element){
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -33,6 +40,15 @@ public class CommonMethods {
     public void clickOnWebElement(WebElement element) {
        waitForElement(element);
         element.click();
+    }
+
+    public void clickElementUsingJavScriptExecutor(WebElement element){
+        waitForElement(element);
+        js.executeScript("arguments[0].click();",element);
+    }
+
+    public void clickElementWithAction(WebElement elementName) {
+        actions.moveToElement(elementName).click().perform();
     }
 
     public void sendKeysInTextBox(WebElement element, String text) {
@@ -68,8 +84,50 @@ public class CommonMethods {
         wait.until(ExpectedConditions.presenceOfElementLocated(element));
     }
 
+    public void invisibilityOfElement(WebElement element) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated((By)element));
+    }
+
+    public boolean isElementDisplayed(WebElement element) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        boolean result;
+        try {
+            element.isDisplayed();
+            result = true;
+        }
+        catch(Exception ex){
+            System.out.println(ex.getStackTrace());
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean isElementEnabled(WebElement element) {
+        return element.isEnabled();
+    }
+
     public String returnText(WebElement element){
         waitForElement(element);
         return element.getText();
+    }
+
+    public void hoverOnElement(WebElement e) {
+        actions.moveToElement(e).build().perform();
+    }
+
+    public boolean verifyTextPresent(WebElement element, String text) {
+        return returnText(element).contains(text);
+    }
+
+    public boolean compareTextEqual(WebElement element, String text) {
+        return returnText(element).equals(text);
+    }
+
+    public boolean verifyForInputValue(WebElement element, String text) {
+        return (element.getAttribute("value").equals(text));
+    }
+
+    public boolean compareTextUsingAttribute(WebElement element, String attributeName, String text) {
+        return (element.getAttribute(attributeName).equals(text));
     }
 }
